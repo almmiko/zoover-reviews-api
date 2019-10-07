75677977fab4682fbc605a454325d7fe1c555cb6
Alexander Mykolaichuk <alexmix3000@gmail.com>
2019-10-07 18:59:02 +0300

    added tests for router

---
+++ __tests__/routes/router.test.js
@@ -0,0 +1,33 @@
+const router = require('../../routes/router');
+
+const supertest = require('supertest');
+const config = require('config');
+const { createApp } = require('../../server');
+
+let server;
+let request;
+
+beforeAll(async (done) => {
+  const app = createApp();
+  server = app.listen(config.PORT, done);
+  request = supertest(server);
+});
+
+afterAll((done) => {
+  server.close(done);
+  console.info('Server closed');
+});
+
+describe('router', () => {
+  test('should return 200 response for reviews stats (/v1/reviews/stats)', async () => {
+    const response = await request.get('/v1/reviews/stats');
+    expect(response.status).toEqual(200);
+  });
+
+  test('should return 200 response for reviews collection (/v1/reviews)', async () => {
+    const response = await request.get('/v1/reviews/stats');
+    expect(response.status).toEqual(200);
+  });
+});
+
+
